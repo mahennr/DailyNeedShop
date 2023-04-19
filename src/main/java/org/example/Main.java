5acb2d5f2511aa52cd86af1782992134de9ea44d
mahennr <mahendratech64@gmail.com>
2023-04-19 13:19:15 +0530

    style 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world! This is Mahendra's Shop ");
+        System.out.println("---------------------------------");
         System.out.println("Welcome to Daily Needs Shop");
     }
 }
