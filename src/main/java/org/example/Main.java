8532fddf7cb227d6ef29192c1b4dc76454167146
mahennr <mahendratech64@gmail.com>
2023-04-19 13:10:54 +0530

    added another sout

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,8 @@
-package org.example;public class Main {
+package org.example;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world! This is Mahendra's Shop ");
+        System.out.println("Welcome to Daily Needs Shop");
     }
 }
